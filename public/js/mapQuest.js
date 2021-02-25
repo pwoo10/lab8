00dd96e7b4174dff99944c0d7353134c569656ea
Pauline Woo <pwoo@ucsd.edu>
2021-02-25 10:24:52 -0800

    final

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'NAn0vdlAoA4oPg1GsDbS0gBmfZZyUh5G';
+
+	var map = L.mapquest.map('map', {
+		center: [32.8785203, -117.2380747],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.8785203, -117.2380747]).addTo(map);
 }
+
